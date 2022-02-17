336285a333c8b763e6402fa9f2d3f15dd5560568
Karina <karinaklevzhits@gmail.com>
2022-02-17 22:28:53 +0300

    ML-007/fix old spec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -6,14 +6,17 @@ RSpec.describe Tag, type: :model do
   context 'with valid data' do
     it { expect(tag).to be_valid }
   end
+
   describe '#name' do
     context 'when name short' do
       subject(:invalid_short_name) { build :tag, :invalid_short_name }
+
       it { expect(invalid_short_name).not_to be_valid }
     end
 
     context 'when name long' do
       subject(:invalid_long_name) { build :tag, :invalid_long_name }
+
       it { expect(invalid_long_name).not_to be_valid }
     end
   end
