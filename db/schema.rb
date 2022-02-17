c98a67ae5a537d4f24c7370802b1ddcbc535cda7
Karina <karinaklevzhits@gmail.com>
2022-02-17 19:10:31 +0300

    ML-008/add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_154516) do
+ActiveRecord::Schema.define(version: 2022_02_17_160237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  
   create_table "companies", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
