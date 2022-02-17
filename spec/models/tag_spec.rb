1824bd533ef3ee5c130daf5950eb4645b4a1a948
Karina <karinaklevzhits@gmail.com>
2022-02-17 18:40:25 +0300

    ML-007/add tests for tag

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  subject(:tag) { create :tag }
+
+  context 'with valid data' do
+    it { expect(tag).to be_valid }
+  end
+  describe '#name' do
+    context 'when name short' do
+      subject(:invalid_short_name) { build :tag, :invalid_short_name }
+      it { expect(invalid_short_name).not_to be_valid }
+    end
+
+    context 'when name long' do
+      subject(:invalid_long_name) { build :tag, :invalid_long_name }
+      it { expect(invalid_long_name).not_to be_valid }
+    end
+  end
+end
