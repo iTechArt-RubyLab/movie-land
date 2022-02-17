58c1d42dc80d9f911b5c89d7ea885b0f84ddbe79
Karina <karinaklevzhits@gmail.com>
2022-02-17 22:34:43 +0300

    ML-008/fix old specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,14 +6,17 @@ RSpec.describe Category, type: :model do
   context 'with valid data' do
     it { expect(category).to be_valid }
   end
+
   describe '#name' do
     context 'when name short' do
       subject(:invalid_short_name) { build :category, :invalid_short_name }
+
       it { expect(invalid_short_name).not_to be_valid }
     end
 
     context 'when name long' do
       subject(:invalid_long_name) { build :category, :invalid_long_name }
+
       it { expect(invalid_long_name).not_to be_valid }
     end
   end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
   context 'with valid attributes' do
     subject(:company) { create :company }
+
     include_examples 'valid model'
   end
+
   describe '#title' do
     context 'when too long' do
       subject(:invalid_long_title) { build :company, :invalid_long_title }
+
       include_examples 'invalid model'
     end
 
     context 'when too short' do
       subject(:invalid_short_title) { build :company, :invalid_short_title }
+
       include_examples 'invalid model'
     end
   end
