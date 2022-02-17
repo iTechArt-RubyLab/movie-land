d46c91f03477a67078a3dc7d0ec1de95100be8fd
forever-alone1 <danalexs27@gmail.com>
2022-02-17 14:06:42 +0300

    ML-004/add genre

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :genres
   resources :countries
   resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
