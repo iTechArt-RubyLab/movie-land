f18e265eab4ee04280146f2d19903e373f460d8f
forever-alone1 <danalexs27@gmail.com>
2022-02-18 13:28:18 +0300

    ML-017/add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, length: { minimum: 2, maximum: 25 }
+  validates :surname, length: { minimum: 2, maximum: 25 }
+  validates :username, presence: true, length: { minimum: 4, maximum: 20 }, uniqueness: true
+  validates :birthday, presence: true
 end
