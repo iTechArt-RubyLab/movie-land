6f22ccaceaa987350923c040f54a900e225d1270
Karina <karinaklevzhits@gmail.com>
2022-02-18 12:31:11 +0300

    ML-015/add factory for people

---
+++ spec/factories/people.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :person do
+    name { Faker::Name.first_name }
+    surname { Faker::Name.last_name }
+    birthday { Faker::Date.between(from: '1999-09-23', to: '2014-09-25') }
+    deathday { Faker::Date.between(from: '2014-09-25', to: '2022-09-25') }
+    association :country
+    married { Faker::Boolean.boolean }
+    association :image
+  end
+  trait :invalid_short_name do
+    title { Faker::Lorem.characters(number: 1, min_alpha: 1) }
+  end
+  trait :invalid_long_name do
+    title { Faker::Lorem.characters(number: 130, min_alpha: 130) }
+  end
+  trait :invalid_short_surname do
+    title { Faker::Lorem.characters(number: 1, min_alpha: 1) }
+  end
+  trait :invalid_long_surname do
+    title { Faker::Lorem.characters(number: 130, min_alpha: 130) }
+  end
+end
