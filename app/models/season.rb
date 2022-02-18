56024b7cc84e02a9470ce17478d116e46a24d2b7
WhatWasThatt <sashakorol584@mail.ru>
2022-02-18 08:22:29 +0200

    ML-014/add season validations

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,4 +1,9 @@
 class Season < ApplicationRecord
   belongs_to :poster, class_name: 'Image'
   belongs_to :series
+
+  validates :title, length: { in: 2..300 }
+  validates :number, numericality: { only_integer: true }, inclusion: { in: 1..100 }
+  validates :description, length: { in: 2..500 }
+  validates :trailer, url: true
 end
