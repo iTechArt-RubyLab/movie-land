7e1e338dfab18530193a2edcf5cf31332a500d17
WhatWasThatt <sashakorol584@mail.ru>
2022-02-18 07:55:02 +0200

    ML-013/add series validations

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,7 @@
 class Series < ApplicationRecord
   belongs_to :poster, class_name: 'Image'
+
+  validates :title, length: { in: 2..300 }
+  validates :description, length: { in: 2..500 }
+  validates :trailer, url: true
 end
