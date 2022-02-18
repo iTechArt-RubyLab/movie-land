454de00a246cbcb31d09aca2c8ed0a46077e472c
Sasha Karol <70864999+WhatWasThatt@users.noreply.github.com>
2022-02-18 20:57:58 +0300

    ML-009/add genre (#11)
    * ML-009/add genre model
    
    * ML-009/add genre migration
    
    * ML-009/add schema
    
    * ML-009/add company validation
    
    * ML-009/add genre factory
    
    * ML-009/add genre model specs

---
+++ spec/factories/genres.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :genre do
+    name { Faker::Book.genre }
+
+    trait :invalid_long_name do
+      name { Faker::Lorem.characters(number: 51) }
+    end
+
+    trait :invalid_short_name do
+      name { Faker::Lorem.characters(number: 1) }
+    end
+  end
+end
---
+++ spec/models/genre_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Genre, type: :model do
+  context 'with valid attributes' do
+    subject(:genre) { create :genre }
+
+    include_examples 'valid model'
+  end
+
+  context 'with invalid attributes' do
+    context 'with name too short' do
+      subject(:invalid_short_name) { build :genre, :invalid_short_name }
+
+      include_examples 'invalid model'
+    end
+
+    context 'with name too long' do
+      subject(:invalid_long_name) { build :genre, :invalid_long_name }
+
+      include_examples 'invalid model'
+    end
+  end
+end
