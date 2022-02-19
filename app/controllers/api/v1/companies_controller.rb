0488a759a4232d3ef6ece874c3837b61249d7893
Karina <karinaklevzhits@gmail.com>
2022-02-20 00:15:45 +0300

    ML-022/add controller to company

---
+++ app/controllers/api/v1/companies_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class CompaniesController < ApplicationController
+      before_action :set_company, only: %i[show update destroy]
+
+      def index
+        @companies = Dictionary::FindService.new(Company, params).call
+
+        render json: @companies, each_serializer: DictionarySerializer
+      end
+
+      def show
+        render json: @company, serializer: DictionarySerializer
+      end
+
+      def create
+        @company = Company.new(company_params)
+
+        if @company.save
+          render json: @company, status: :created, serializer: DictionarySerializer
+        else
+          render json: @company.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @company.update(company_params)
+          render json: @company, serializer: DictionarySerializer
+        else
+          render json: @company.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @company.destroy
+      end
+
+      private
+
+      def set_company
+        @company = Company.find(params[:id])
+      end
+
+      def company_params
+        params.require(:company).permit(%i[name])
+      end
+    end
+  end
+end
