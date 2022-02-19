1a69fa6505398342a77279e858f6d7ec2621b40b
WhatWasThatt <sashakorol584@mail.ru>
2022-02-19 21:12:57 +0200

    ML-021/add genre routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :countries
       resources :companies
+      resources :genres
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
