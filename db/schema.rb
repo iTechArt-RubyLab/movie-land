6154e9a6d495e5882d47c11a9b0d2876913217d3
WhatWasThatt <sashakorol584@mail.ru>
2022-02-19 22:17:51 +0200

    ML-020/update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_075219) do
+ActiveRecord::Schema.define(version: 2022_02_19_201606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "countries", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_075219) do
   end
 
   create_table "movies", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.text "description"
     t.string "tagline"
     t.string "trailer"
