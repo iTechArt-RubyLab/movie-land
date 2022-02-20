0ef6e55a630a506e6f305d9cde45f684c57f3334
Danya Gulyuta <70888057+twill11@users.noreply.github.com>
2022-02-21 00:46:55 +0300

    ML-017/add authentication (#27)
    * ML-017/add devise-token-auth gem
    
    * ML-017/add devise config
    
    * ML-017/add devise user
    
    * ML-017/add rack-cors config

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  include DeviseTokenAuth::Concerns::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :lockable, :confirmable, :trackable
+end
