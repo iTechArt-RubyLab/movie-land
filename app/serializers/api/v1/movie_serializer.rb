a4e8bff9b217b34a7a6871310622d3c477587d51
WhatWasThatt <sashakorol584@mail.ru>
2022-02-20 15:03:48 +0200

    Ml-023/add associations to serializer

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -3,6 +3,10 @@ module Api
     class MovieSerializer < ActiveModel::Serializer
       attributes :id, :name, :description, :tagline, :trailer,
                  :release_date, :age_limit, :budget, :duration
+      has_many :companies, serializer: DictionarySerializer
+      has_many :countries, serializer: DictionarySerializer
+      has_many :genres, serializer: DictionarySerializer
+      has_many :languages, serializer: DictionarySerializer
     end
   end
 end
