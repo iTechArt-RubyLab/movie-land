1ee494edf8e4adb5eda3e6d85e2351866632cdb1
WhatWasThatt <sashakorol584@mail.ru>
2022-02-21 00:45:56 +0200

    ML-024/add role validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,6 @@
 class Role < ApplicationRecord
   has_one :permission, dependent: :destroy
+  validates :name, uniqueness: { scope: :name, message: 'This role already exists' }
 
   accepts_nested_attributes_for :permission
 end
