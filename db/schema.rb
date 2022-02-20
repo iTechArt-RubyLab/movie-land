602b2d8916c69c12ae3b107aeaf6f11b1fefdda2
Karina <karinaklevzhits@gmail.com>
2022-02-20 15:45:52 +0300

    ML-011/add schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2022_02_19_201606) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.text "description"
