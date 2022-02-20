1253158e2d665f2a8016aa5956aefb8976e0303c
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-02-20 17:36:30 +0300

    ML-011/add language (#12)
    * ML-011/add language model
    
    * ML-011/add validations for language
    
    * ML-011/add factory for language
    
    * ML-011/add tests for language
    
    * ML-011/fix old specs
    
    * ML-011/add controller to language
    
    * ML-011/add route for language
    
    * ML-011/add tests for controller
    
    * ML-011/add tests for route
    
    * ML-011/add schema
    
    * ML-011/add temp seed for language

---
+++ spec/factories/languages.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :language do
+    name { Faker::Nation.language }
+
+    trait :invalid_short_name do
+      name { Faker::Lorem.characters(number: 1, min_alpha: 1) }
+    end
+    trait :invalid_long_name do
+      name { Faker::Lorem.characters(number: 50, min_alpha: 50) }
+    end
+  end
+end
---
+++ spec/requests/api/v1/language_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+module Api
+  module V1
+    RSpec.describe '/languages', type: :request do
+      let(:valid_attributes) do
+        attributes_for :language
+      end
+
+      let(:invalid_attributes) do
+        attributes_for :language, :invalid_short_name
+      end
+
+      let(:valid_headers) do
+        { 'ACCEPT' => 'application/json' }
+      end
+
+      describe 'GET /index' do
+        it 'renders a successful response' do
+          Language.create! valid_attributes
+          get api_v1_languages_url, headers: valid_headers, as: :json
+          expect(response).to be_successful
+        end
+      end
+
+      describe 'GET /show' do
+        it 'renders a successful response' do
+          language = Language.create! valid_attributes
+          get api_v1_language_url(language), as: :json
+          expect(response).to be_successful
+        end
+      end
+
+      describe 'POST /create' do
+        context 'with valid parameters' do
+          it 'creates a new Language' do
+            expect do
+              post api_v1_languages_url,
+                   params: { language: valid_attributes }, headers: valid_headers, as: :json
+            end.to change(Language, :count).by(1)
+          end
+        end
+
+        context 'with invalid parameters' do
+          it 'does not create a new Language' do
+            expect do
+              post api_v1_languages_url,
+                   params: { language: invalid_attributes }, as: :json
+            end.to change(Language, :count).by(0)
+          end
+        end
+      end
+
+      describe 'PATCH /update' do
+        context 'with valid parameters' do
+          let(:new_attributes) do
+            attributes_for :language
+          end
+
+          it 'updates the requested Language' do
+            language = Language.create! valid_attributes
+            patch api_v1_language_url(language),
+                  params: { language: new_attributes }, headers: valid_headers, as: :json
+            language.reload
+            attributes_for :language
+          end
+        end
+      end
+
+      describe 'DELETE /destroy' do
+        it 'destroys the requested language' do
+          language = Language.create! valid_attributes
+          expect do
+            delete api_v1_language_url(language), headers: valid_headers, as: :json
+          end.to change(Language, :count).by(-1)
+        end
+      end
+    end
+  end
+end
