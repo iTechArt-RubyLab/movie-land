d0ba187c4c3e51d69722983281efdb16610818ff
Ilya Berinchik <ilyabyar@gmail.com>
2022-02-21 00:19:54 +0300

    Fix Rubocop offenses.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,9 +1,9 @@
 class Country < ApplicationRecord
   # rubocop:disable Rails/HasManyOrHasOneDependent
   has_many :countries_movies
-  # rubocop:enable Rails/HasManyOrHasOneDependent
   has_many :movies, through: :countries_movies, dependent: :destroy
 
   has_many :people
+  # rubocop:enable Rails/HasManyOrHasOneDependent
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
 end
