211cc925b4d337b5865574253fb6b23dfd967019
Karina <karinaklevzhits@gmail.com>
2022-02-20 16:08:25 +0300

    ML-015/add connection country with people

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,5 +2,6 @@ class Country < ApplicationRecord
   has_many :countries_movies
   has_many :movies, through: :countries_movies, dependent: :destroy
 
+  has_many :people
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
 end
