9bd198301cb1b47e71b295a6de4691100838ef21
Karina <karinaklevzhits@gmail.com>
2022-02-20 17:23:10 +0300

    ML-015/add route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :genres
       resources :languages
       resources :movies
+      resources :people
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
