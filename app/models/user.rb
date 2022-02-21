36f062d89a284f932f408b29d019c025cbbd8218
Ilya Berinchik <ilyabyar@gmail.com>
2022-02-21 11:06:04 +0300

    Fix Rubocop.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   extend Devise::Models
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
