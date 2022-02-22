68e6b2e4d31b991b33c41d6bc66396023433b76a
forever-alone1 <danalexs27@gmail.com>
2022-02-23 00:26:07 +0300

    ML-029/fix user add_index migration

--- db/migrate/20220220190037_devise_token_auth_create_users.rb
+++ db/migrate/20220220190037_devise_token_auth_create_users.rb
@@ -53,6 +53,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
-    add_index :users, :username,             unique: true
   end
 end
