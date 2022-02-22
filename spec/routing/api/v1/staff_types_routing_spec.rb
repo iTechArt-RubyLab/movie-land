018c39b537ed6503bd06cd205e0d4a8ba9c6dda9
Karina <karinaklevzhits@gmail.com>
2022-02-22 23:30:44 +0300

    ML-027/add specs for staff type route

---
+++ spec/routing/api/v1/staff_types_routing_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+module Api
+  module V1
+    RSpec.describe Api::V1::StaffTypesController, type: :routing do
+      describe 'routing' do
+        it 'routes to #index' do
+          expect(get: 'api/v1/staff_types').to route_to('api/v1/staff_types#index')
+        end
+
+        it 'routes to #show' do
+          expect(get: 'api/v1/staff_types/1').to route_to('api/v1/staff_types#show', id: '1')
+        end
+
+        it 'routes to #create' do
+          expect(post: 'api/v1/staff_types').to route_to('api/v1/staff_types#create')
+        end
+
+        it 'routes to #update via PUT' do
+          expect(put: 'api/v1/staff_types/1').to route_to('api/v1/staff_types#update', id: '1')
+        end
+
+        it 'routes to #update via PATCH' do
+          expect(patch: 'api/v1/staff_types/1').to route_to('api/v1/staff_types#update', id: '1')
+        end
+
+        it 'routes to #destroy' do
+          expect(delete: 'api/v1/staff_types/1').to route_to('api/v1/staff_types#destroy', id: '1')
+        end
+      end
+    end
+  end
+end
