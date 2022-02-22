f9cdf9a8ccdeb3f1ac607f9ef05bfd85574e036d
forever-alone1 <danalexs27@gmail.com>
2022-02-22 21:19:41 +0300

    ML-029/add rating serializer and service

---
+++ app/service/rating_service.rb
@@ -0,0 +1,22 @@
+class RatingService
+  def initialize(class_name, params)
+    @class_name = class_name
+    @params = params
+  end
+
+  def call
+    if params[:filter].present?
+      class_name.where('rating ILIKE ?', "%#{params[:filter]}%").paginate(page: params[:page]).order("rating #{order}")
+    else
+      class_name.paginate(page: params[:page]).order("rating #{order}")
+    end
+  end
+
+  private
+
+  attr_accessor :class_name, :params
+
+  def order
+    params[:order].presence || 'asc'
+  end
+end
