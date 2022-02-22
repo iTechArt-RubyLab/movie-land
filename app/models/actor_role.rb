ed599f123cfe92a16a5ec514d115ee0fa9aff372
Karina <karinaklevzhits@gmail.com>
2022-02-23 02:31:33 +0300

    ML-027/add comments to models

--- app/models/actor_role.rb
+++ app/models/actor_role.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: actor_roles
+#
+#  id         :bigint           not null, primary key
+#  role_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  movie_id   :bigint           not null
+#  person_id  :bigint           not null
+#
+# Indexes
+#
+#  index_actor_roles_on_movie_id   (movie_id)
+#  index_actor_roles_on_person_id  (person_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (movie_id => movies.id)
+#  fk_rails_...  (person_id => people.id)
+#
 class ActorRole < ApplicationRecord
   belongs_to :person
   belongs_to :movie
--- app/models/movie_staff.rb
+++ app/models/movie_staff.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: movie_staffs
+#
+#  id            :bigint           not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  movie_id      :bigint           not null
+#  person_id     :bigint           not null
+#  staff_type_id :bigint           not null
+#
+# Indexes
+#
+#  index_movie_staffs_on_movie_id       (movie_id)
+#  index_movie_staffs_on_person_id      (person_id)
+#  index_movie_staffs_on_staff_type_id  (staff_type_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (movie_id => movies.id)
+#  fk_rails_...  (person_id => people.id)
+#  fk_rails_...  (staff_type_id => staff_types.id)
+#
 class MovieStaff < ApplicationRecord
   belongs_to :movie
   belongs_to :person
