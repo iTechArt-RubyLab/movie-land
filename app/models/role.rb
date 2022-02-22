67517342192ccd0f8d4cb0f1117b3b27a906ba74
WhatWasThatt <sashakorol584@mail.ru>
2022-02-22 02:30:27 +0200

    ML-024/update permissions

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,29 +2,33 @@ class Role < ApplicationRecord
   has_one :permission, dependent: :destroy
   has_many :users, dependent: :nullify
   validates :name, uniqueness: { message: 'This role already exists' }
-  accepts_nested_attributes_for :permission, reject_if: proc { |attributes| attributes.blank? }
+  accepts_nested_attributes_for :permission
 
   def can_read?
-    predefined_user? ? true : permission.can_read
+    predefined_user? ? true : permision? && permission.can_read
   end
 
   def can_lock_user?
-    admin? ? true : permission.can_lock_user
+    admin? ? true : permision? && permission.can_lock_user
   end
 
   def can_edit_role?
-    admin? ? true : permission.can_edit_role
+    admin? ? true : permision? && permission.can_edit_role
   end
 
   def can_set_role?
-    admin? ? true : permission.can_set_role
+    admin? ? true : permision? && permission.can_set_role
   end
 
   def can_edit?
-    editor? ? true : permission.can_set_role
+    editor? ? true : permision? && permission.can_set_role
   end
 
   private
+
+  def permision?
+    !permission.nil?
+  end
 
   def admin?
     name == 'admin'
