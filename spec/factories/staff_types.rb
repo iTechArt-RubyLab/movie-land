b32221456ec0f49b0633d7fe68eb031d97e18202
Karina <karinaklevzhits@gmail.com>
2022-02-22 21:56:06 +0300

    ML-027/add factory for staff type

---
+++ spec/factories/staff_types.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :staff_type do
+    name { Faker::Emotion.unique.noun }
+
+    trait :invalid_short_name do
+      name { Faker::Lorem.characters(number: 1, min_alpha: 1) }
+    end
+    trait :invalid_long_name do
+      name { Faker::Lorem.characters(number: 35, min_alpha: 35) }
+    end
+  end
+end
