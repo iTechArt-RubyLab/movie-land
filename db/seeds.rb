9b861cddb8a68afb6b672edb02bdab83fb0a8ec1
forever-alone1 <danalexs27@gmail.com>
2022-02-23 01:23:57 +0300

    ML-025/improve seeds with user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,26 @@ FactoryBot.create_list(:genre, 20)
 FactoryBot.create_list(:language, 20)
 FactoryBot.create_list(:movie, 20)
 FactoryBot.create_list(:person, 20)
-Role.create(name: 'admin', permission_attributes: { can_lock_user: true, can_edit_role: true, can_set_role: true, can_read: true, can_edit: true })
-Role.create(name: 'redactor', permission_attributes: { can_lock_user: false, can_edit_role: false, can_set_role: false, can_read: true, can_edit: true })
-Role.create(name: 'reviewer', permission_attributes: { can_lock_user: false, can_edit_role: false, can_set_role: false, can_read: true, can_edit: false })
-FactoryBot.create_list(:tag, 20)
-FactoryBot.create_list(:user, 5)
+admin_role = Role.create(name: 'admin', permission_attributes:
+                        { can_lock_user: true, can_edit_role: true,
+                          can_set_role: true, can_read: true, can_edit: true })
+redactor_role = Role.create(name: 'redactor', permission_attributes:
+                        { can_lock_user: false, can_edit_role: false,
+                          can_set_role: false, can_read: true, can_edit: true })
+reviewer_role = Role.create(name: 'reviewer', permission_attributes:
+                        { can_lock_user: false, can_edit_role: false,
+                          can_set_role: false, can_read: true, can_edit: false })
+
+User.create(email: 'admin@example.com', password: '123456', password_confirmation: '123456',
+            name: Faker::Name.first_name, surname: Faker::Name.last_name,
+            username: Faker::Internet.unique.username(specifier: 8), birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
+            role_id: admin_role.id, confirmed_at: Time.zone.now)
+
+User.create(email: 'redactor@example.com', password: '123456', password_confirmation: '123456',
+            name: Faker::Name.first_name, surname: Faker::Name.last_name,
+            username: Faker::Internet.unique.username(specifier: 8), birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
+            role_id: redactor_role.id)
+User.create(email: 'reviewer@example.com', password: '123456', password_confirmation: '123456',
+            name: Faker::Name.first_name, surname: Faker::Name.last_name,
+            username: Faker::Internet.unique.username(specifier: 8), birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
+            role_id: reviewer_role.id)
