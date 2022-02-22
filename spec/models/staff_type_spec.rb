45bd9b694d313a4ae10383800b0cea951d56150e
Karina <karinaklevzhits@gmail.com>
2022-02-22 21:59:22 +0300

    ML-027/add specs for model staff type

--- spec/models/staff_type_spec.rb
+++ spec/models/staff_type_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe StaffType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:staff_type) { create :staff_type }
+
+  context 'with valid data' do
+    it { expect(staff_type).to be_valid }
+  end
+
+  describe '#name' do
+    context 'when name short' do
+      subject(:invalid_short_name) { build :staff_type, :invalid_short_name }
+
+      it { expect(invalid_short_name).not_to be_valid }
+    end
+
+    context 'when name long' do
+      subject(:invalid_long_name) { build :staff_type, :invalid_long_name }
+
+      it { expect(invalid_long_name).not_to be_valid }
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:name).with_message('This staff type already exists') }
+  end
 end
