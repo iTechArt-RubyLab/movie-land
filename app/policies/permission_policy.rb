116fe1fbc3797f98e9b88e903999656b327d3cbd
WhatWasThatt <sashakorol584@mail.ru>
2022-02-23 00:24:19 +0200

    ML-028/make permission policy inherited from application policy

--- app/policies/permission_policy.rb
+++ app/policies/permission_policy.rb
@@ -1,2 +1,43 @@
-class PermissionPolicy < RolePolicy
+class PermissionPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    super(user)
+    super(record)
+  end
+
+  def index?
+    user.can_read?
+  end
+
+  def show?
+    user.can_read?
+  end
+
+  def create?
+    user.can_edit_role?
+  end
+
+  def update?
+    user.can_edit_role?
+  end
+
+  def destroy?
+    user.can_edit_role?
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
 end
