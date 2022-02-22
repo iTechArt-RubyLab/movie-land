d1c50ebdcff86a3c8ea8baaf40fd64ffc9a5c18a
WhatWasThatt <sashakorol584@mail.ru>
2022-02-22 18:23:35 +0200

    ML-026/update migrations

--- db/migrate/20220221234643_add_index_to_role.rb
+++ db/migrate/20220221234643_add_index_to_role.rb
@@ -1,5 +1,7 @@
 class AddIndexToRole < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
   def change
-    add_index :roles, :name, unique: true
+    add_index :roles, :name, unique: true, algorithm: :concurrently
   end
 end
