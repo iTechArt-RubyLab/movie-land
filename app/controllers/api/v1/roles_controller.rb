74852a8cbd6776746abd858c963692d213ea0b9c
WhatWasThatt <sashakorol584@mail.ru>
2022-02-28 23:33:51 +0200

    ML-034/remove rubocop line length rule

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -38,9 +38,11 @@ module Api
       end
 
       def role_params
-        params.require(:role).permit(:name, permission_attributes: %i[can_lock_user can_edit_role can_edit_permission can_read_rating can_give_rating
-                                                                      can_set_role can_edit_movie can_read_movie can_edit_person can_read_person
-                                                                      can_read_entities can_edit_entities can_read_user])
+        params.require(:role).permit(:name, permission_attributes: %i[can_lock_user can_edit_role can_edit_permission
+                                                                      can_read_rating can_give_rating can_set_role
+                                                                      can_edit_movie can_read_movie can_edit_person
+                                                                      can_read_person can_read_entities
+                                                                      can_edit_entities can_read_user])
       end
     end
   end
