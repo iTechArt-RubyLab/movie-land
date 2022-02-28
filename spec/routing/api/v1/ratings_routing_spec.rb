67121e04bf5fcb6c8eec2c0f6475a227ab9c00ed
forever-alone1 <danalexs27@gmail.com>
2022-03-01 02:28:59 +0300

    ML-036/improve routing specs

--- spec/routing/api/v1/ratings_routing_spec.rb
+++ spec/routing/api/v1/ratings_routing_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe Api::V1::RatingsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/api/v1/ratings').to route_to('api/v1/ratings#index')
+      expect(get: '/api/v1/my-ratings').to route_to('api/v1/ratings#index')
     end
 
     it 'routes to #create' do
-      expect(post: '/api/v1/ratings').to route_to('api/v1/ratings#create')
+      expect(post: '/api/v1/my-ratings').to route_to('api/v1/ratings#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/api/v1/ratings/1').to route_to('api/v1/ratings#update', id: '1')
+      expect(put: '/api/v1/my-ratings/1').to route_to('api/v1/ratings#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/api/v1/ratings/1').to route_to('api/v1/ratings#update', id: '1')
+      expect(patch: '/api/v1/my-ratings/1').to route_to('api/v1/ratings#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/api/v1/ratings/1').to route_to('api/v1/ratings#destroy', id: '1')
+      expect(delete: '/api/v1/my-ratings/1').to route_to('api/v1/ratings#destroy', id: '1')
     end
   end
 end
--- spec/routing/api/v1/view_lists_routing_spec.rb
+++ spec/routing/api/v1/view_lists_routing_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe Api::V1::ViewListsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: 'api/v1/view_lists').to route_to('api/v1/view_lists#index')
+      expect(get: 'api/v1/my-view-list').to route_to('api/v1/view_lists#index')
     end
 
     it 'routes to #create' do
-      expect(post: 'api/v1/view_lists').to route_to('api/v1/view_lists#create')
+      expect(post: 'api/v1/my-view-list').to route_to('api/v1/view_lists#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: 'api/v1/view_lists/1').to route_to('api/v1/view_lists#update', id: '1')
+      expect(put: 'api/v1/my-view-list/1').to route_to('api/v1/view_lists#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: 'api/v1/view_lists/1').to route_to('api/v1/view_lists#update', id: '1')
+      expect(patch: 'api/v1/my-view-list/1').to route_to('api/v1/view_lists#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: 'api/v1/view_lists/1').to route_to('api/v1/view_lists#destroy', id: '1')
+      expect(delete: 'api/v1/my-view-list/1').to route_to('api/v1/view_lists#destroy', id: '1')
     end
   end
 end
