be77066711e0eb54ecbbd0771b21b2e5b9ed8a73
WhatWasThatt <sashakorol584@mail.ru>
2022-03-02 01:48:38 +0200

    ML-031/update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,10 @@ FactoryBot.create_list(:language, 20)
 FactoryBot.create_list(:movie, 20)
 FactoryBot.create_list(:person, 20)
 FactoryBot.create_list(:rating, 20)
-FactoryBot.create_list(:permission, 1, :admin)
-FactoryBot.create_list(:permission, 1, :redactor)
-FactoryBot.create_list(:permission, 1, :reviewer)
-User.create(email: 'admin@example.com', password: '123456', password_confirmation: '123456',
-            name: Faker::Name.first_name, surname: Faker::Name.last_name,
-            username: Faker::Internet.unique.username(specifier: 8),
-            birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
-            role: Role.first, confirmed_at: Time.zone.now)
-User.create(email: 'redactor@example.com', password: '123456', password_confirmation: '123456',
-            name: Faker::Name.first_name, surname: Faker::Name.last_name,
-            username: Faker::Internet.unique.username(specifier: 8),
-            birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
-            role: Role.second, confirmed_at: Time.zone.now)
-User.create(email: 'reviewer@example.com', password: '123456', password_confirmation: '123456',
-            name: Faker::Name.first_name, surname: Faker::Name.last_name,
-            username: Faker::Internet.unique.username(specifier: 8),
-            birthday: Faker::Date.birthday(min_age: 18, max_age: 65),
-            role: Role.third, confirmed_at: Time.zone.now)
+FactoryBot.create(:permission, :admin)
+FactoryBot.create(:permission, :redactor)
+FactoryBot.create(:permission, :reviewer)
+FactoryBot.create(:user, :admin, email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+FactoryBot.create(:user, :redactor, email: 'redactor@example.com', password: '123456', password_confirmation: '123456')
+FactoryBot.create(:user, :reviewer, email: 'reviewer@example.com', password: '123456', password_confirmation: '123456')
 FactoryBot.create_list(:rating, 20)
