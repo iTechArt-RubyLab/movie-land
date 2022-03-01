7ba622320aec6e72d1f995faa96da57681270ffc
WhatWasThatt <sashakorol584@mail.ru>
2022-03-01 22:02:24 +0200

    ML-038/remove protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::API
   include Pundit::Authorization
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :null_session, unless: -> { request.format.json? }
   include Error::ErrorHandler
 
   protected
