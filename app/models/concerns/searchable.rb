8e77a73f382a8efd7c4878231903a132d65d2470
WhatWasThatt <sashakorol584@mail.ru>
2022-03-02 19:52:51 +0200

    ML-042/add custom index with autocomplete

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,5 +4,73 @@ module Searchable
   included do
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
+
+    def as_indexed_json(_options = {})
+      as_json(only: %i[name])
+    end
+
+    settings settings_attributes do
+      mappings dynamic: false do
+        indexes :name, type: :text, analyzer: :autocomplete
+      end
+    end
+
+    def self.search(query, _filters)
+      set_filters = lambda do |context_type, filter|
+        @search_definition[:query][:bool][context_type] |= [filter]
+      end
+
+      @search_definition = {
+        size: 5,
+        query: {
+          bool: {
+            must: [],
+            should: [],
+            filter: []
+          }
+        }
+      }
+
+      if query.blank?
+        set_filters.call(:must, match_all: {})
+      else
+        set_filters.call(
+          :must,
+          match: {
+            name: {
+              query: query,
+              fuzziness: 1
+            }
+          }
+        )
+      end
+
+      __elasticsearch__.search(@search_definition)
+    end
+  end
+
+  class_methods do
+    def settings_attributes
+      {
+        index: {
+          analysis: {
+            analyzer: {
+              autocomplete: {
+                type: :custom,
+                tokenizer: :standard,
+                filter: %i[lowercase autocomplete]
+              }
+            },
+            filter: {
+              autocomplete: {
+                type: :edge_ngram,
+                min_gram: 2,
+                max_gram: 25
+              }
+            }
+          }
+        }
+      }
+    end
   end
 end
