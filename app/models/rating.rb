885977615eb8830d89e7767abfa1f008b38c4fc0
forever-alone1 <danalexs27@gmail.com>
2022-03-03 03:20:15 +0300

    ML-036/improve uniqueness ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -11,9 +11,9 @@
 #
 # Indexes
 #
-#  index_ratings_on_movie_id             (movie_id)
-#  index_ratings_on_rating_and_movie_id  (rating,movie_id) UNIQUE
-#  index_ratings_on_user_id              (user_id)
+#  index_ratings_on_movie_id              (movie_id)
+#  index_ratings_on_user_id               (user_id)
+#  index_ratings_on_user_id_and_movie_id  (user_id,movie_id) UNIQUE
 #
 # Foreign Keys
 #
@@ -25,5 +25,5 @@ class Rating < ApplicationRecord
   belongs_to :movie
 
   validates :rating, numericality: { only_integer: true }, inclusion: { in: 1..10 }
-  validates :rating, uniqueness: { scope: :movie_id, message: 'This movie already rated by you.' }
+  validates :user_id, uniqueness: { scope: :movie_id, message: 'This movie already rated by you.' }
 end
--- db/migrate/20220228182310_add_index_to_ratings.rb
+++ db/migrate/20220228182310_add_index_to_ratings.rb
@@ -2,6 +2,6 @@ class AddIndexToRatings < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_index :ratings, [:rating, :movie_id], unique: true, algorithm: :concurrently
+    add_index :ratings, [:user_id, :movie_id], unique: true, algorithm: :concurrently
   end
 end
