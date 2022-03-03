5c06af9854adac53ee4f2f6cc32a6412bc945b19
Karina <karinaklevzhits@gmail.com>
2022-03-04 01:31:35 +0300

    ML-040/add specs for mailers

---
+++ spec/mailers/redactor_mailer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe RedactorMailer, type: :mailer do
+  describe 'redactor notification' do
+    let!(:ratings) { create_list(:rating, 20).pluck(:rating, :movie_id, :user_id) }
+    let!(:redactor) { create :user, :redactor }
+    let(:mail) { described_class.redactor_notification(ratings, redactor).deliver }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Redactor Notification')
+    end
+
+    it 'renders the redactor' do
+      expect(mail.to).to eq([redactor.email])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hello')
+    end
+
+    it 'sends an email' do
+      described_class.redactor_notification(ratings, redactor).deliver
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+  end
+end
---
+++ spec/mailers/reviewer_mailer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ReviewerMailer, type: :mailer do
+  describe 'reviewer notification' do
+    let!(:movies) { create_list(:movie, 20).pluck(:name, :release_date) }
+    let!(:reviewer) { create :user, :reviewer }
+    let(:mail) { described_class.reviewer_notification(movies, reviewer).deliver }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Reviewer Notification')
+    end
+
+    it 'renders the reviewer' do
+      expect(mail.to).to eq([reviewer.email])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hello')
+    end
+
+    it 'sends an email' do
+      described_class.reviewer_notification(movies, reviewer).deliver
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+  end
+end
