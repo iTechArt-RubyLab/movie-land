294cd6768fd0f04add0dd62fe7e3e8914db1f4da
WhatWasThatt <sashakorol584@mail.ru>
2022-03-04 22:31:11 +0200

    ML-046/add validation for comment body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,4 +23,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :delete_all
+
+  validates :body, length: { in: 2..2000 }
 end
