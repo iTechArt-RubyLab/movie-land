7443903bec83cd02ce59c26c56efb308c14e2bea
Karina <karinaklevzhits@gmail.com>
2022-03-04 09:07:51 +0300

    ML-040/delete previews

--- spec/mailers/previews/reviewer_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-class ReviewerMailerPreview < ActionMailer::Preview
-  def reviewer_notification
-    movies = Movie.where(release_date: (Time.zone.today..Time.zone.today + 7)).pluck(:name, :release_date)
-    reviewer = User.where(role_id: 3).first
-    ReviewerMailer.reviewer_notification(movies, reviewer)
-  end
-end
