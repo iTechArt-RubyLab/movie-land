74e08a499503ec46c5aad8b974a0026287605d33
WhatWasThatt <sashakorol584@mail.ru>
2022-03-04 21:19:52 +0200

    ML-046/add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 2022_03_06_152341) do
     t.index ["name"], name: "index_awards_on_name", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -238,7 +249,11 @@ ActiveRecord::Schema.define(version: 2022_03_06_152341) do
 
   add_foreign_key "actor_roles", "movies"
   add_foreign_key "actor_roles", "people", column: "actor_id"
+<<<<<<< HEAD
   add_foreign_key "awards", "countries"
+=======
+  add_foreign_key "comments", "users"
+>>>>>>> ML-046/add comment model
   add_foreign_key "movie_staffs", "movies"
   add_foreign_key "movie_staffs", "people", column: "staff_id"
   add_foreign_key "people", "countries"
