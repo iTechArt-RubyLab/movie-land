d63271aab1070625188ad2329fdb7d25e613238f
WhatWasThatt <sashakorol584@mail.ru>
2022-03-05 01:00:17 +0200

    ML-046/imrpove routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,33 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
-      resources :countries
-      resources :companies
-      resources :genres
-      resources :languages
+      resources :countries, only: %i[index create update destroy]
+      resources :companies, only: %i[index create update destroy]
+      resources :genres, only: %i[index create update destroy]
+      resources :languages, only: %i[index create update destroy]
+
       resources :movies do
-        resources :comments
+        resources :comments, only: %i[create update destroy]
       end
-      resources :comments do
-        resources :comments
+
+      resources :comments, only: %i[create update destroy] do
+        resources :comments, only: %i[create update destroy]
       end
+
       resources :people do
-        resources :comments
+        resources :comments, only: %i[create update destroy]
       end
+
       resources :roles do
-        resource :permissions
+        resource :permissions, only: %i[show create update]
       end
-      resources :tags
-      resources :user do
+
+      resources :tags, only: %i[index]
+
+      resources :user, only: %i[index show update] do
         put 'access', to: 'access_user#update'
       end
+      
       resources :view_lists, as: :my_view_list, path: 'my-view-list'
       resources :ratings, as: :my_ratings, path: 'my-ratings'
       resources :awards
