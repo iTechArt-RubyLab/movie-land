07e5854a63ba7566edb1971cb7d6e1002ab2d98e
WhatWasThatt <sashakorol584@mail.ru>
2022-03-06 20:11:39 +0200

    ML-043/add movie award migration

---
+++ db/migrate/20220306172539_create_movie_awards.rb
@@ -0,0 +1,13 @@
+class CreateMovieAwards < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movie_awards do |t|
+      t.string :name
+      t.integer :delivery_year
+      t.integer :nomination_type
+      t.references :movie, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -102,6 +102,17 @@ ActiveRecord::Schema.define(version: 2022_03_06_152341) do
     t.index ["movie_id", "language_id"], name: "index_languages_movies_on_movie_id_and_language_id", unique: true
   end
 
+  create_table "movie_awards", force: :cascade do |t|
+    t.string "name"
+    t.integer "delivery_year"
+    t.bigint "movie_id", null: false
+    t.bigint "award_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["award_id"], name: "index_movie_awards_on_award_id"
+    t.index ["movie_id"], name: "index_movie_awards_on_movie_id"
+  end
+
   create_table "movie_staffs", force: :cascade do |t|
     t.integer "staff_type", default: 0, null: false
     t.bigint "movie_id", null: false
@@ -252,6 +263,8 @@ ActiveRecord::Schema.define(version: 2022_03_06_152341) do
   add_foreign_key "actor_roles", "people", column: "actor_id"
   add_foreign_key "awards", "countries"
   add_foreign_key "comments", "users"
+  add_foreign_key "movie_awards", "awards"
+  add_foreign_key "movie_awards", "movies"
   add_foreign_key "movie_staffs", "movies"
   add_foreign_key "movie_staffs", "people", column: "staff_id"
   add_foreign_key "people", "countries"
