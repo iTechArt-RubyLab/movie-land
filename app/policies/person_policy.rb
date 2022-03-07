b0c92c099a66bbeed631707d5f3d2de67831f96e
forever-alone1 <danalexs27@gmail.com>
2022-03-07 20:00:36 +0300

    ML-047/update policies with guest user

--- app/policies/person_policy.rb
+++ app/policies/person_policy.rb
@@ -2,11 +2,11 @@ class PersonPolicy < ApplicationPolicy
   attr_reader :user, :record
 
   def index?
-    user.can_read_person?
+    user.can_read_person? || user.guest?
   end
 
   def show?
-    user.can_read_person?
+    user.can_read_person? || user.guest?
   end
 
   def create?
