dcba87afcd42a3e2be6a04a1fa48f6d0f3febcbf
WhatWasThatt <sashakorol584@mail.ru>
2022-03-07 23:35:59 +0200

    ML-043/add movie award seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,17 @@ FactoryBot.create_list(:view_list, 20)
 FactoryBot.create_list(:comment, 20)
 FactoryBot.create_list(:comment, 20, :for_comment)
 FactoryBot.create_list(:comment, 20, :for_person)
+awards = []
+['Oscar', 'Golden globe', 'British akademy',
+ 'Actors Guild', 'Nika', 'Golden Eagle',
+ 'Goya', 'Emmy', 'Cesar', 'Saturn'].each do |award|
+  awards << FactoryBot.create(:award, name: award)
+end
+['Best movie', 'Best sound', 'Best adapted screenplay', 'The best music',
+ 'Best cast', 'Best editing', 'Best original soundtrack', 'Best screenplay',
+ 'Best costumes', 'Best visual effects', 'Best makeup', 'Best scenery',
+ 'Best action scene', 'Best operator performance', 'Best sound editing'].each do |movie_award|
+  awards.each do |award|
+    FactoryBot.create(:movie_award, name: movie_award, award: award)
+  end
+end
