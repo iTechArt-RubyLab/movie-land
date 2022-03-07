00deec9652d6755e9d2c09867db06acbb33ee2a4
forever-alone1 <danalexs27@gmail.com>
2022-03-07 20:09:34 +0300

    ML-047/improve deleted migration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,7 +7,9 @@
 #  budget       :bigint
 #  description  :text
 #  duration     :integer
+#  images       :string           default([]), is an Array
 #  name         :string
+#  poster       :string
 #  release_date :date
 #  tagline      :string
 #  trailer      :string
--- db/migrate/20220306152341_add_can_give_view_list_to_permissions.rb
+++
@@ -1,5 +0,0 @@
-class AddCanGiveViewListToPermissions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :permissions, :can_give_view_list, :boolean
-  end
-end
