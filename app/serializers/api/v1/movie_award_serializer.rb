db1ec480a4b1c32a6cc5f7b41fe0d160e0d56166
WhatWasThatt <sashakorol584@mail.ru>
2022-03-09 01:59:41 +0200

    ML-043/update serializers

--- app/serializers/api/v1/movie_award_serializer.rb
+++ app/serializers/api/v1/movie_award_serializer.rb
@@ -4,6 +4,7 @@ module Api
       attributes :id, :nomination_type, :delivery_year
       belongs_to :movie, serializer: MovieSerializer
       belongs_to :category, serializer: CategorySerializer
+      has_many :people, serializer: PersonSerializer
     end
   end
 end
