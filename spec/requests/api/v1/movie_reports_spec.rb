62cf71916884b203667c3e83b846be683a1a0c51
WhatWasThatt <sashakorol584@mail.ru>
2022-03-11 18:45:21 +0200

    ML-045/add reports request specs

---
+++ spec/requests/api/v1/movie_reports_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe '/movie_reports', type: :request do
+  let(:admin) { create :user, :admin }
+  let(:valid_headers) { sign_in admin }
+
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      create_list(:movie, 3)
+      get "#{api_v1_movie_reports_url}.csv", headers: valid_headers, as: :csv
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      movie = create(:movie)
+      get "#{api_v1_movie_movie_report_url(movie_id: movie.id)}.csv", headers: valid_headers, as: :csv
+      expect(response).to be_successful
+    end
+  end
+end
