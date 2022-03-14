4ca3b69d45abde24e22b4988f02a854eecb30a05
Karina <karinaklevzhits@gmail.com>
2022-03-14 07:53:13 +0300

    ML-049/move services

---
+++ spec/services/entity_manager/person_service_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe EntityManager::PersonService, type: :model do
+  subject(:person_service) { described_class.new(params) }
+
+  let!(:people) { create_list(:person, 20) }
+
+  describe '#call' do
+    context 'when params contain name one of people' do
+      let!(:person) { people.first }
+      let(:params) { { filter: person.name } }
+
+      it 'returns person' do
+        expect(person_service.call).eql? person
+      end
+    end
+
+    context 'when params contain surname one of people' do
+      let!(:person) { people.second }
+      let(:params) { { filter: person.surname } }
+
+      it 'returns person' do
+        expect(person_service.call).eql? person
+      end
+    end
+
+    context 'when params contain name of non-existent person' do
+      let(:params) { { filter: Faker::Lorem.characters(number: 7) } }
+
+      it 'returns count of people' do
+        expect(person_service.call.count).to eq 0
+      end
+    end
+
+    context 'when params contain order by desc' do
+      let(:params) { { order: :desc } }
+
+      it 'returns people' do
+        expect(person_service.call).eql? people.sort_by(&:name).reverse
+      end
+    end
+
+    context 'when params is empty and default order by asc' do
+      let(:params) { {} }
+
+      it 'returns people' do
+        expect(person_service.call).eql? people.sort_by(&:name)
+      end
+    end
+  end
+end
