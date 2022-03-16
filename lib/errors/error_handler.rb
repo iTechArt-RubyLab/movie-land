b1a19ff6c24700f50e03b94978ee4059866e70ea
WhatWasThatt <sashakorol584@mail.ru>
2022-03-16 20:02:42 +0200

    ML-054/update error handling module

--- lib/errors/error_handler.rb
+++ lib/errors/error_handler.rb
@@ -1,9 +1,9 @@
-module Error
+module Errors
   module ErrorHandler
     def self.included(clazz)
       clazz.class_eval do
-        rescue_from StandardError do |e|
-          respond(:standard_error, 500, e.to_s)
+        rescue_from StandardError do |error|
+          respond(:standard_error, 500, error.to_s)
         end
 
         rescue_from ArgumentError do |error|
