31ac1743b9ec55d0f950522c59fdb2d1f475c4be
forever-alone1 <danalexs27@gmail.com>
2022-03-17 15:31:26 +0300

    ML-044/improve retry limit faker

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -24,7 +24,7 @@
 #
 FactoryBot.define do
   factory :movie do
-    name { Faker::Movie.unique.title }
+    sequence(:name) { |var| Faker::Movie.title + var.to_s }
     description { Faker::Lorem.sentence(word_count: 5) }
     tagline { Faker::Movie.quote }
     trailer { Faker::Internet.url }
